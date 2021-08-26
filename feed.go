67ef4a92f9000b28c3b91d98c6555293a63eb608
Alexey Yerin <yyp@disroot.org>
2021-08-26 23:38:44 +0300

    feed: clean-up post visibility
    Also fixes inconsistent colors of users depending whether they
    do or don't have a display name.

--- feed.go
+++ feed.go
@@ -99,15 +99,12 @@ func showTootOptions(app *App, status *mastodon.Status, showSensitive bool) (str
 
 	head += reblogText
 
-	showedVisibility := false
+	head += fmt.Sprintf(special1+"(%s) ", status.Visibility)
 	if status.Account.DisplayName != "" {
-		showedVisibility = true
-		head += fmt.Sprintf(special1+"(%s) %s%s\n", status.Visibility, special2, status.Account.DisplayName)
-	}
-	if !showedVisibility {
-		head += fmt.Sprintf(special2+"(%s) %s%s\n\n", status.Visibility, special1, status.Account.Acct)
+		head += fmt.Sprintf("%s%s\n", special2, status.Account.DisplayName)
+		head += fmt.Sprintf("%s%s\n\n", special1, status.Account.Acct)
 	} else {
-		head += fmt.Sprintf(special1+"%s\n\n", status.Account.Acct)
+		head += fmt.Sprintf("%s%s\n\n", special2, status.Account.Acct)
 	}
 	output := head
 	content := stripped
