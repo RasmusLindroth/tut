0a82504e313754e2286eda7634fd3c96d83f25b5
Omar Polo <op@omarpolo.com>
2022-12-06 20:42:34 +0000

    delete temp files after viewing them

--- ui/media.go
+++ ui/media.go
@@ -147,9 +147,19 @@ func openMediaType(tv *TutView, filenames []string, mediaType string) {
 		for _, ext := range external {
 			exec.Command(ext.Name, ext.Args...).Run()
 		}
+		deleteFiles(filenames)
 	}()
 	for _, term := range terminal {
 		openInTerminal(tv, term.Name, term.Args...)
+	}
+	if len(terminal) != 0 {
+		deleteFiles(filenames)
+	}
+}
+
+func deleteFiles(filenames []string) {
+	for _, filename := range filenames {
+		os.Remove(filename)
 	}
 }
 
