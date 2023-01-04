28795244f38afa050ddf6d8f1340611e4648a942
Rasmus Lindroth <rasmus@lindroth.xyz>
2023-01-04 18:57:13 +0100

    show chars left

--- ui/editorview.go
+++ ui/editorview.go
@@ -1,8 +1,11 @@
 package ui
 
 import (
+	"fmt"
+
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
+	"github.com/rivo/uniseg"
 )
 
 type EditorView struct {
@@ -11,6 +14,7 @@ type EditorView struct {
 	View      *tview.Flex
 	editor    *tview.TextArea
 	controls  *tview.Flex
+	info      *tview.TextView
 	limit     int
 	prevPage  string
 	prevFocus tview.Primitive
@@ -24,6 +28,7 @@ func NewEditorView(tv *TutView) *EditorView {
 		shared:   tv.Shared,
 		editor:   NewTextArea(tv.tut.Config),
 		controls: NewControlView(tv.tut.Config),
+		info:     NewTextView(tv.tut.Config),
 	}
 	e.View = editorViewUI(e)
 	return e
@@ -34,25 +39,47 @@ func editorViewUI(e *EditorView) *tview.Flex {
 	if e.tutView.tut.Config.General.TerminalTitle < 2 {
 		r.AddItem(e.shared.Top.View, 1, 0, false)
 	}
-	r.AddItem(e.editor, 0, 1, false).
-		AddItem(e.controls, 1, 0, false).
-		AddItem(e.shared.Bottom.View, 2, 0, false)
+	if e.limit > 0 {
+		r.AddItem(e.info, 1, 0, false).
+			AddItem(e.editor, 0, 1, false).
+			AddItem(e.controls, 1, 0, false).
+			AddItem(e.shared.Bottom.View, 2, 0, false)
+	} else {
+		r.AddItem(e.editor, 0, 1, false).
+			AddItem(e.controls, 1, 0, false).
+			AddItem(e.shared.Bottom.View, 2, 0, false)
+	}
 	return r
 }
 
 func (e *EditorView) Init(text string, textLimit int, setReturn bool, exit func(string)) {
 	e.editor.SetText(text, true)
 	e.limit = textLimit
+	*e.View = *editorViewUI(e)
 	e.exitFunc = exit
 	if setReturn {
 		e.prevPage, _ = e.tutView.View.GetFrontPage()
 		e.prevFocus = e.tutView.tut.App.GetFocus()
 		e.prevInput = e.tutView.tut.App.GetInputCapture()
 	}
+	e.info.SetText("")
+	e.editor.SetChangedFunc(e.updateInfo)
+	e.updateInfo()
 	e.tutView.View.HidePage(e.prevPage)
 	e.tutView.View.ShowPage("editor")
 	e.tutView.tut.App.SetInputCapture(e.tutView.InputEditorView)
 	e.tutView.tut.App.SetFocus(e.editor)
+}
+
+func (e *EditorView) updateInfo() {
+	if e.limit > 0 {
+		content := e.editor.GetText()
+		charCount := uniseg.GraphemeClusterCount(content)
+		charsLeft := e.limit - charCount
+		e.info.SetText(
+			fmt.Sprintf("Chars left: %d", charsLeft),
+		)
+	}
 }
 
 func (e *EditorView) ExitTextAreaInput() {
